f9ae6b6400c3b48482b5335a2ade1eba1a0161e1
moaath14 <chwrha@gmail.com>
2024-10-23 22:11:00 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = 'توكن'
-const id = 'ايدي حسابك'
+const token = '7195729535:AAF8rB6MTy0dWR2lgvBLrQfet_Ce2YM-fTk'
+const id = '6595549074'
 const address = 'https://www.google.com'
 
 const app = express();
